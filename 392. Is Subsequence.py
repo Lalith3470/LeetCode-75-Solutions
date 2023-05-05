2a457f60840cf9d69bcb5545ace644aef3467fa3
Lalith Kiran Molleti <101722978+Lalith3470@users.noreply.github.com>
2023-05-05 21:21:32 +0530

    Create 392. Is Subsequence.py

---
+++ 392. Is Subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+
+        count = 0
+        for i in t:
+            if i == s[count]:
+                count+=1
+            if count== len(s):
+                break
+        return count==len(s)
